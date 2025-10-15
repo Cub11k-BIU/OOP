fbac3a3dd46eac66a1dd8fb1bd6266c22acf3de1
Cub11k <ostashenkokd@gmail.com>
2025-10-15 18:14:21 +0300

    Assignment template

---
+++ src/MainTemplate.java
@@ -0,0 +1,15 @@
+/**
+ * MainTemplate.
+ *
+ * @author Konstantin Ostashenko
+ * © 2025
+ */
+public class MainTemplate {
+    /**
+     * Main method template.
+     *
+     * @param args command line arguments
+     * */
+    public static void main(String[] args) {
+    }
+}
